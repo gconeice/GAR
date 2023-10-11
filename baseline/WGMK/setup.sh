c38c534019f8cf277e4d7cf2234d172d2eeeecd3
gconeice <yyang811@gatech.edu>
2023-10-11 11:48:13 -0400

    final

--- baseline/WGMK/setup.sh
+++ baseline/WGMK/setup.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y build-essential openjdk-8-jdk git gcc-mipsel-linux-gnu g++-mips-linux-gnu llvm &&
+sudo apt-get install -y build-essential openjdk-8-jdk git gcc-mipsel-linux-gnu g++-mips-linux-gnu llvm iftop iproute &&
 sudo mkdir -p /opt/mipsel/usr/bin/ &&
 sudo ln -s /usr/bin/mipsel-linux-gnu-gcc /opt/mipsel/usr/bin/mipsel-linux-gnu-gcc &&
 sudo ln -s /usr/bin/llvm-objdump /opt/mipsel/usr/bin/mipsel-linux-gnu-llvm-objdump
